0a2b7353e9643ceb8023e293976aee970fc7c297
NickPham3245 <nicholas2513.email@gmail.com>
2025-12-14 19:32:34 -0800

    Added recipes

--- ripeCode.py
+++ ripeCode.py
@@ -1,4 +1,4 @@
-import jetson_inference, jetson_utils
+import jetson_inference, jetson_utils, random
 
 #Step 1: Load image
 #Step 2: Loading the model
@@ -9,6 +9,19 @@ cameraFeed = jetson_utils.videoSource("/dev/video0")
 showFeed = jetson_utils.videoOutput("webrtc://@:8554/output")
 prevLabel = None
 model = jetson_inference.imageNet(model="resnet18.onnx", labels="labels.txt", input_blob="input_0", output_blob="output_0")
+foodType = {"Apples":"fruit",
+            "Banana":"fruit",
+            "Bellpepper":"vegetable",
+            "Carrot":"vegetable",
+            "Cucumber":"vegetable",
+            "Mango":"fruit",
+            "Oranges":"fruit",
+            "Potato":"vegetable",
+            "Strawberry":"fruit",
+            "Tomato":"vegetable",}
+
+recipes = {"fruit":["Cut and eat","Make smoothie","Make juice"],
+           "vegetable":["Make a salad","Make a soup","Make a casserole"]}
 
 while True:
     frame = cameraFeed.Capture()
@@ -23,6 +36,10 @@ while True:
         
         if label != prevLabel:
             print(label, confidence*100)
+            if label.startswith("Rotten"):
+                print("You should probably throw this away.")
+            else:
+                print(random.choice(recipes[foodType[label[6:]]]))
+                
         prevLabel=label
             
-

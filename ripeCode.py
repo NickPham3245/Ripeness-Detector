6bbec76473f68ad2539979e7731b99a9c91390eb
NickPham3245 <nicholas2513.email@gmail.com>
2025-12-20 17:32:17 -0800

    Added label to feed

--- ripeCode.py
+++ ripeCode.py
@@ -6,8 +6,9 @@ import jetson_inference, jetson_utils, random
 #Step 4: Get+display results
 
 cameraFeed = jetson_utils.videoSource("/dev/video0")
-showFeed = jetson_utils.videoOutput("webrtc://@:8554/output")
+showFeed = jetson_utils.videoOutput("feed.mp4")
 prevLabel = None
+font = jetson_utils.cudaFont()
 model = jetson_inference.imageNet(model="resnet18.onnx", labels="labels.txt", input_blob="input_0", output_blob="output_0")
 foodType = {"Apples":"fruit",
             "Banana":"fruit",
@@ -30,12 +31,15 @@ while True:
         index, confidence = model.Classify(frame)
 
         label = model.GetClassLabel(index)
-
+        confidence=confidence*100
+        font.OverlayText(frame, text=f"{confidence:05.2f}% {label}", 
+                         x=5, y=5 * (font.GetSize() + 5),
+                         color=font.White, background=font.Gray40)
         
         showFeed.Render(frame)
         
         if label != prevLabel:
-            print(label, confidence*100)
+            print(label, confidence)
             if label.startswith("Rotten"):
                 print("You should probably throw this away.")
             else:
